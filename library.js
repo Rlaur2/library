7dab78163f3f69fb4e5559a946955a5abfa65362
Rlaur2 <plaureanopena@gmail.com>
2022-05-03 23:54:41 -0400

    Finish submit and close functionality

--- library.js
+++ library.js
@@ -1,12 +1,24 @@
 const myLibrary = [];
 
+/*sections of the DOM*/
 const shelf = document.querySelector('.shelf');
-const addBook = document.querySelector('.add-book');
 const bookForm = document.querySelector('.extra-container');
+
+/*buttons on the page*/
+const addBook = document.querySelector('.add-book');
 /*Testing to see if more advanced selectors also work in JS.
 The answer is yes, the selector below works!*/
 const submitBook = document.querySelector('.book-form div:nth-child(6)');
 const closeButton = document.querySelector('.close');
+
+/*the user inputs on the page*/
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const genreInput = document.querySelector('#genre');
+const yesRadio = document.querySelector('#yes');
+const noRadio = document.querySelector('#no');
+const partialRadio = document.querySelector('#partially');
 
 const CreateBook = function(title, author, pages, read, genre) {
     this.title = title;
@@ -35,16 +47,21 @@ const removeFromLibrary = (book) => {
 
 const displayBooks = () => {
     myLibrary.forEach((book) => {
+        if (book.displayed) {
+            return;
+        }
         let bookContainer = document.createElement('div');
         let title = document.createElement('div');
         let author = document.createElement('div');
         let pages = document.createElement('div');
         let genre = document.createElement('div');
         let read = document.createElement('div');
-        if (book.read) {
+        if (book.read === 'yes') {
             readStatus = 'This book has been read.'
-        } else {
+        } else if (book.read === 'no' ) {
             readStatus = 'This book has not been read.'
+        } else {
+            readStatus = 'This book has been partially read.'
         };
         title.textContent = `Title: ${book.title}`;
         author.textContent = `Author: ${book.author}`
@@ -56,7 +73,8 @@ const displayBooks = () => {
         bookContainer.appendChild(pages);
         bookContainer.appendChild(genre);
         bookContainer.appendChild(read);
-        shelf.appendChild(bookContainer); 
+        shelf.appendChild(bookContainer);
+        book.displayed = true; 
     })
 }
 
@@ -65,18 +83,66 @@ addBook.addEventListener('click', () => {
 });
 
 submitBook.addEventListener('click', () => {
-   
+    if (!titleInput.value) {
+        titleInput.classList.add('error');
+    } else {
+        titleInput.classList.remove('error');
+    } if (!authorInput.value) {
+        authorInput.classList.add('error');
+    } else {
+        authorInput.classList.remove('error');
+    } if (!pagesInput.value) {
+        pagesInput.classList.add('error');
+    } else {
+        pagesInput.classList.remove('error');
+    } if (!genreInput.value) {
+        genreInput.classList.add('error');
+    } else {
+        genreInput.classList.remove('error');
+    } if (titleInput.className.includes('error') && !titleInput.value  ||
+        authorInput.className.includes('error')  && !authorInput.value ||
+        pagesInput.className.includes('error')   && !pagesInput.value ||
+        genreInput.className.includes('error')   && !genreInput.value) {
+          alert('Missing info');
+          return;
+    }title = titleInput.value;
+     author = authorInput.value;
+     pages = pagesInput.value;
+     genre = genreInput.value;
+    if (yesRadio.checked) {
+         read = 'yes';
+    } else if (noRadio.checked) {
+         read = 'no';
+    } else {
+         read = 'partial'
+    };  newBook = new CreateBook(title,author,pages,read,genre);
+    addToLibrary(newBook);
+    displayBooks();
+    reset();
 });
+
+const reset = () => { 
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
+    genreInput.value = '';
+    yesRadio.checked = true;
+};
 
 closeButton.addEventListener('click', () => {
     bookForm.classList.add('hidden');
+    reset();
+    titleInput.classList.remove('error');
+    authorInput.classList.remove('error');
+    pagesInput.classList.remove('error');
+    genreInput.classList.remove('error');
 });
 
-/* testing below */
+/* testing below 
 const maus = new CreateBook ('Maus','Art Spiegelman',205,false,'Historical Drama');
 const vendetta = new CreateBook ('V For Vendetta','Alan Moore',165, true, 'Alternative History');
 const ready = new CreateBook ('Ready Player One','Ernest Clive',308,true,'Youth Adventure');
 addToLibrary(maus);
 addToLibrary(vendetta);
 addToLibrary(ready);
-displayBooks();
+displayBooks();*/
